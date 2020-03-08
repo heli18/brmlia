e04a0f009e94669ca7b03b431034dd9382fefc2f
NickWhalen <nicholas.j.whalen@gmail.com>
2020-03-08 01:15:00 +0100

    basic working version with tiff only

---
+++ brmlia-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <br/>
+      <h2>Select Images for Upload</h2>
+      <div id="app"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ brmlia-app/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tiff from 'tiff.js';
+import './index.css';
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+      imagePreviewUrl: '',
+      imageCanvas: '',
+      canvasHeight: '',
+      canvasWidth: ''
+    };
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    reader.onloadend = () => {
+      var buffer = reader.result;
+      var tiff = new Tiff({buffer: buffer});
+      var width = tiff.width();
+      var height = tiff.height();
+      let canvasImage = tiff.toCanvas();
+      document.getElementById("output").append(canvasImage);
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result,
+        imageCanvas: canvasImage,
+        canvasWidth: width,
+        canvasHeight: height
+      });
+    }
+    reader.readAsArrayBuffer(file);
+  }
+
+  render() {
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput"
+            type="file"
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton"
+            type="submit"
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+        <div id="output" className="imgPreview">
+        </div>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<ImageUpload/>, document.getElementById("root"));
