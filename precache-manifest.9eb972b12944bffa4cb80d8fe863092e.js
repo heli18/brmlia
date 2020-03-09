ec68491d9de82bcb0796226b9fd07b2d1b08ffd9
Heidi Li <hel779@g.harvard.edu>
2020-03-09 12:18:42 -0500

    Updates

---
+++ precache-manifest.9eb972b12944bffa4cb80d8fe863092e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f2e74ac7d9f0e22c800",
+    "url": "/brmlia/static/css/main.c15447e1.chunk.css"
+  },
+  {
+    "revision": "4f2e74ac7d9f0e22c800",
+    "url": "/brmlia/static/js/main.2d0db68c.chunk.js"
+  },
+  {
+    "revision": "925a03781ff868ab1e2e",
+    "url": "/brmlia/static/js/runtime~main.091645ed.js"
+  },
+  {
+    "revision": "cf1f2a4eccfaca2b94ec",
+    "url": "/brmlia/static/css/2.564fddba.chunk.css"
+  },
+  {
+    "revision": "cf1f2a4eccfaca2b94ec",
+    "url": "/brmlia/static/js/2.b2772c20.chunk.js"
+  },
+  {
+    "revision": "2aa73ce9919eb7f5c8fbfe32d393cf71",
+    "url": "/brmlia/index.html"
+  }
+];
