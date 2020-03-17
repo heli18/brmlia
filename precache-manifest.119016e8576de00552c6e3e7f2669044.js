37b34f2d583bbe20b4f586832bf38c701fa7abeb
nverno <noah.v.peart@gmail.com>
2020-03-17 16:53:53 -0400

    Updates

---
+++ precache-manifest.119016e8576de00552c6e3e7f2669044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad057996c8549c756a23b73edf440a85",
+    "url": "/brmlia/index.html"
+  },
+  {
+    "revision": "2bba25c1ac0e41afbe80",
+    "url": "/brmlia/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "a17f4c7ef3fc518d456c",
+    "url": "/brmlia/static/css/main.3328dd13.chunk.css"
+  },
+  {
+    "revision": "2bba25c1ac0e41afbe80",
+    "url": "/brmlia/static/js/2.fb3aedf5.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/brmlia/static/js/2.fb3aedf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17f4c7ef3fc518d456c",
+    "url": "/brmlia/static/js/main.f21f1244.chunk.js"
+  },
+  {
+    "revision": "c1c3ce5dfd26e552c790",
+    "url": "/brmlia/static/js/runtime-main.47a9004c.js"
+  }
+]);
