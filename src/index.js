806eb6111d50d9b8fa5fda19038ea19926f6cd52
Heidi Li <hel779@g.harvard.edu>
2020-03-20 14:46:37 -0500

    Comments in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Scene} from "./imagecanvas/Test.js"
+import Scene from "./imagecanvas/Test.js"
 import TestWrapper from "./imagecanvas/TestWrapper.js"
 import TestFunctionWrapper from "./imagecanvas/TestFunctionWrapper.js"
 import TestFunctionWrapperWrapper from "./imagecanvas/TestFunctionWrapperWrapper.js"
@@ -8,8 +8,11 @@ import TestReactWrapper from "./imagecanvas/TestReactWrapper.js"
 
 function Application() {
   return (
-    /*<TestFunctionWrapperWrapper />*/
-    <TestReactWrapper />
+    /*<Scene />*/ // doesn't work - because it's not wrapped in Canvas
+    /*<TestWrapper />*/ // doesn't work - because it's not wrapped in Canvas
+    /*<TestFunctionWrapper />*/ // works
+    /*<TestFunctionWrapperWrapper />*/ // works
+    <TestReactWrapper /> //works
   );
 }
 
